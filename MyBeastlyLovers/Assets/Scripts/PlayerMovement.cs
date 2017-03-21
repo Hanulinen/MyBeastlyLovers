b206ef8b2d0354a7d6b1b574803d9ad4d5b14730
soyhan <soyhan15@gmail.com>
2017-03-21 18:18:56 +0200

    walking
    walking added

---
+++ MyBeastlyLovers/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+
+    [SerializeField]
+    int MovementSpeed;
+
+
+    Rigidbody2D rbody;
+    Animator anim;
+   
+	void Start () {
+        rbody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();   
+	}
+	
+
+	void Update () {
+
+        Vector2 movement_vector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        if (movement_vector != Vector2.zero)
+        {
+            anim.SetBool("iswalking", true);
+            anim.SetFloat("input_x", movement_vector.x);
+            anim.SetFloat("input_y", movement_vector.y);
+        } else {
+            anim.SetBool("iswalking", false);
+        }
+        rbody.MovePosition(rbody.position + movement_vector * Time.deltaTime * MovementSpeed);
+
+
+	}
+
+
+
+}
