2835dda7f412ee482a67130c0dd1298158a81b7c
soyhan <soyhan15@gmail.com>
2017-03-22 23:29:25 +0200

    Map Complete
    Implemented the buildings. Added collisions. Added warping triggers.

---
+++ MyBeastlyLovers/Assets/Scripts/Warp.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Warp : MonoBehaviour {
+
+    public Transform warpTarget;
+    
+    private void OnTriggerEnter2D(Collider2D target)
+    {
+        if (target.transform.CompareTag("Player"))
+        {
+            target.gameObject.transform.position = warpTarget.position;
+            Camera.main.transform.position = warpTarget.position;
+        }
+    }
+}
